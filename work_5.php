f24a660bb5994b82219500e7b6e8ec760a5bff03
SunnyCoveDD <dimakhov174@gmail.com>
2022-03-12 15:31:39 +0500

    version 1.0.4

---
+++ work_5.php
@@ -0,0 +1,6 @@
+<?php
+function reverse($arg1)
+{
+    echo "Норм строка - " . $arg1 . "<br> Перевернутая строка - " . strrev($arg1);
+}
+reverse("asd");
---
+++ work_6.php
@@ -0,0 +1,7 @@
+<?php
+$arr = [33,22,11,55,'wm-school'];
+function massive($arg1)
+{
+    echo asort($arg1);
+}
+massive($arr);
